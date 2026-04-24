9c597e40ad4aef14457d309956dbcbd7df6e091c
shravani1206 <navaleshravani6@gmail.com>
2026-04-24 21:16:11 +0530

    fixed app initialization error

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,14 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 
+const app = express();
+app.use(cors());
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/login.html"));
 });
-const app = express();
-app.use(cors());
-app.use(express.json());
+
 
 console.log("✅ BACKEND SERVER RUNNING");
 // MongoDB
